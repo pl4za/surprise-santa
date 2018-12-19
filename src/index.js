1991fa6c903a10c878c23ef4723da331bdc8f272
Jason Costa <jasonphilipsardocosta@gmail.com>
2018-12-19 14:39:55 +0000

    Hide element on animation end

--- src/index.js
+++ src/index.js
@@ -9,22 +9,23 @@ export default class ExampleComponent extends Component {
 
   componentDidMount = () => {
     let getRandomNumber = (min, max) => Math.random() * (max - min) + min;
-    let time = getRandomNumber(2, 10);
     let currentAnimation = 'santa-move-right';
-
-    let updateAnimation = () => currentAnimation === 'santa-move-right' ?
+    let updateCurrentAnimation = () => currentAnimation === 'santa-move-right' ?
       currentAnimation = 'santa-move-left' : currentAnimation = 'santa-move-right';
 
+    let time = 5;
     let animate = () => {
       let santa = document.querySelector('.santa');
+      santa.style.display = 'block';
       santa.style.setProperty('--animation', currentAnimation);
       santa.style.setProperty('--from', getRandomNumber(0, 100) + '%');
       santa.style.setProperty('--to', getRandomNumber(0, 100) + '%');
       santa.style.setProperty('--animation-time', time + 's');
-      updateAnimation();
+      updateCurrentAnimation();
+      setTimeout(() => santa.style.display = 'none', time * 1000);
     };
 
-    setInterval(animate, time * 1000);
+    setInterval(animate, getRandomNumber(time, 10) * 1000);
   }
 
   render() {
