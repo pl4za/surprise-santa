ade60cf6d2a836abf453ddb7c4252bc88b321a6d
Jason Costa <jason.costa@starlingbank.com>
2021-11-09 11:38:00 +0000

    Tweak example page

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import SurpriseSanta from 'surprise-santa'
+import logo from './santa.gif';
+import './App.css';
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <SurpriseSanta minTime={5} maxTime={60}/>
+      <div className="App">
+        <SurpriseSanta minTime={5} maxTime={60} />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          Merry 0 Christmas!
+          <br/>
+          (Santa is coming)
+        </header>
       </div>
     )
   }
